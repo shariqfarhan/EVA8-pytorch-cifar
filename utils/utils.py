59eab347a1f8ee6940f166f8c98c3fc0b47d6eb4
shariqfarhan <57046534+shariqfarhan@users.noreply.github.com>
2023-03-08 16:00:38 +0530

    Updated with GradCAM functions

--- utils/utils.py
+++ utils/utils.py
@@ -13,6 +13,12 @@ import matplotlib.pyplot as plt
 import albumentations as A
 from tqdm import tqdm
 
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image, deprocess_image, preprocess_image
+import cv2
+
 
 def get_mean_and_std(dataset):
     '''Compute the mean and std value of dataset.'''
@@ -140,3 +146,101 @@ class AlbumentationImageDataset(Dataset):
             image = self.norm(image = np.array(image))['image']
         image = np.transpose(image, (2,0,1)).astype(np.float32)
         return torch.tensor(image, dtype = torch.float), label    
+
+
+def plot_train_test_acc_loss(train_losses, train_acc, test_losses, test_acc):
+    t = [t.cpu().item() for t in train_losses]
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(t)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
+    
+    
+def plot_misclassified_images(incorr_X, incorr_y, incorr_argmax, classes):
+    fig = plt.figure(figsize=(20, 8))
+    fig.suptitle("CIFAR Mis-Classified Images", fontsize=15)
+    for idx in range(10):
+        ax = fig.add_subplot(2, 5, idx+1, xticks=[], yticks=[])
+        img = np.squeeze(incorr_X[idx])
+        img = img.permute(1, 2, 0)
+        label = incorr_y[idx]
+        img_pred = incorr_argmax[idx]
+        plt.imshow(img.cpu().numpy(), cmap="gray")
+        ax.set_title(f"Prediction : {classes[img_pred]} \n (label: {classes[label]})")
+
+def plot_gradcam(model, images, labels, predictions, classes ,layer, device, num_of_images=10):
+    fig = plt.figure(figsize=(32, 32))
+    nrow = num_of_images
+    ncol = 3
+    fig, axes = plt.subplots(nrow, ncol, figsize=(32,32), squeeze=False)
+    
+    for idx in range(num_of_images):
+        '''
+        images - list of images to be processed
+        labels - what is the ground truth for the image
+        predictions - what the model has been predicting
+        layer - GradCAM for the required layer
+        '''
+        # Given an image, we denormalize it. 
+        # As this is CIFAR dataset, the below means and stds are for this specific data set
+        input_tensor = images[idx]
+        input_tensor = torch.tensor(input_tensor)
+        denorms = denormalize(input_tensor, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+        # We unsqueeze to add the batch parameter to the tensor
+        input_tensor = torch.unsqueeze(denorms, 0)
+        input_tensor = input_tensor.permute(0,3, 2, 1) 
+        # This step is to permute the data in the required input format
+        # We move the channels from end to second input - 3 --> 0
+        input_tensor = input_tensor.to(device)
+        img = input_tensor
+        
+        targets = [ClassifierOutputTarget(labels[idx])]
+        target_layers = layer
+        with GradCAM(model=model, target_layers=target_layers) as cam:
+            grayscale_cams = cam(input_tensor=input_tensor, targets=targets)
+            img = torch.squeeze(img,0)
+            img = img.permute(2,1,0)
+            img = img.cpu().numpy()
+            cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
+        cam = np.uint8(255*grayscale_cams[0, :])
+        cam = cv2.merge([cam, cam, cam])
+        img = cv2.resize(img, (224, 224))
+        cam = cv2.resize(cam, (224, 224))
+        cam_image = cv2.resize(cam_image, (224, 224))
+
+        label = labels[idx]
+        img_pred = predictions[idx]
+        
+        # Show the Original Image
+        ax = axes[idx, 0]
+        ax.imshow(img)
+        ax.set_title(f"Original Image: {classes[label]}")
+        ax.axis('off')
+        
+        ax = axes[idx, 1]
+        ax.imshow(cam)
+        ax.set_title(f"GradCAM Plot for Prediction: {classes[img_pred]})")
+        ax.axis('off')
+        
+        ax = axes[idx, 2]
+        ax.imshow(cam_image)
+        ax.set_title(f"Prediction : {classes[img_pred]} \n (label: {classes[label]})")
+        ax.axis('off')
+        
+    plt.tight_layout()
+    plt.show()
+
+def denormalize(x, mean, std):
+    # 3, H, W, B
+    ten = x.clone().permute(1, 2, 0)
+    for t, m, s in zip(ten, mean, std):
+        t.mul_(s).add_(m)
+    # B, 3, H, W
+    return torch.clamp(ten, 0, 1).permute( 0, 1, 2)
+    
